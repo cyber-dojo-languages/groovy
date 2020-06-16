29af79c98656c6133ecb748a890fa394d4637b32
Jon Jagger <jon@jaggersoft.com>
2020-06-16 11:12:57 +0100

    Upgrade to 3.0.3

--- check_version.sh
+++ check_version.sh
@@ -6,7 +6,7 @@ readonly JSON=`cat docker/image_name.json`
 readonly IMAGE_NAME="${BASH_REMATCH[1]}"
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
-readonly EXPECTED=2.4.12
+readonly EXPECTED=3.0.3
 readonly ACTUAL=$(docker run --rm -it ${IMAGE_NAME} sh -c 'groovyc --version')
 
 if echo "${ACTUAL}" | grep -q "${EXPECTED}"; then

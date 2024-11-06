e8d99b52bc64e014e111b3d1504df4c01abb541b
Faye <faye@kosli.com>
2024-11-06 14:43:47 +0100

    Update to groovy 4.0.23, move to Github Actions and GHCR

--- check_version.sh
+++ check_version.sh
@@ -6,7 +6,7 @@ readonly JSON=`cat docker/image_name.json`
 readonly IMAGE_NAME="${BASH_REMATCH[1]}"
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
-readonly EXPECTED=4.0.9
+readonly EXPECTED=4.0.23
 readonly ACTUAL=$(docker run --rm -it ${IMAGE_NAME} sh -c 'groovyc --version')
 
 if echo "${ACTUAL}" | grep -q "${EXPECTED}"; then
